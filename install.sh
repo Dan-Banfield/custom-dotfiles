211998f5667d9c7bf108081588984f2aa00cda6d
Dan Banfield <dandbanfield@gmail.com>
2026-05-31 15:30:19 +0100

    [UPDATE] Add screenshot to clipboard keybind, and update dependencies in installation script.

--- install.sh
+++ install.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
 echo "🚀 Installing system dependencies..."
-# Core desktop packages
-sudo pacman -S --needed hyprland kitty dolphin stow git github-cli swww rofi
+sudo pacman -S --needed hyprland kitty dolphin stow git swww rofi hyprshot
 
 echo "📦 Installing AUR dependencies (Themes & Fixes)..."
-# Using yay for AUR packages
 paru -S --needed bibata-cursor-theme-bin
 
 echo "🔗 Symlinking configuration files via Stow..."
 cd ~/.dotfiles
 stow hypr
 
-echo "✅ Setup complete! Restart your system to see changes."
+echo "✅ Setup complete!"
+
+echo "Restarting in 5..."
+sleep 1
+echo "Restarting in 4..."
+sleep 1
+echo "Restarting in 3..."
+sleep 1
+echo "Restarting in 2..."
+sleep 1
+echo "Restarting in 1..."
+sleep 1
+systemctl reboot
